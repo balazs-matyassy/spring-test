c6c71e1c81c338bdc1c64bb4cb8ab7cbe5277b55
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-25 17:28:31 +0200

    balazs-matyassy: update

---
+++ src/main/java/hu/progmatic/springtest/command/MainRunner.java
@@ -0,0 +1,10 @@
+package hu.progmatic.springtest.command;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MainRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
--- src/main/java/hu/progmatic/springtest/service/ExpenditureSummarizer.java
+++ src/main/java/hu/progmatic/springtest/service/ExpenditureSummarizer.java
@@ -1,5 +1,31 @@
 package hu.progmatic.springtest.service;
 
-public class ExpenditureSummarizer {
+import hu.progmatic.springtest.model.Category;
+import hu.progmatic.springtest.model.Currency;
+import hu.progmatic.springtest.model.Expenditure;
 
+import java.time.LocalDate;
+import java.util.List;
+
+public class ExpenditureSummarizer {
+    public double getExpenditureSum(List<Expenditure> expenditures, Currency currency) {
+        return 0.0;
+    }
+
+    public double getExpenditureSum(
+            List<Expenditure> expenditures,
+            Currency currency,
+            Category category
+    ) {
+        return 0.0;
+    }
+
+    public double getExpenditureSum(
+            List<Expenditure> expenditures,
+            Currency currency,
+            LocalDate dateFrom,
+            LocalDate dateTo
+    ) {
+        return 0.0;
+    }
 }
