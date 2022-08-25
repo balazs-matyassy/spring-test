0ea6ed6be5a2378eb1390517dd8dea6655fe4ac2
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-25 20:05:24 +0200

    balazs-matyassy: Mocks

--- src/test/java/hu/progmatic/springtest/command/MainRunnerTest.java
+++ src/test/java/hu/progmatic/springtest/command/MainRunnerTest.java
@@ -3,13 +3,29 @@ package hu.progmatic.springtest.command;
 import hu.progmatic.springtest.model.Category;
 import hu.progmatic.springtest.model.Currency;
 import hu.progmatic.springtest.service.ExpenditureSummarizer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class MainRunnerTest {
+
+    @Mock
+    private ExpenditureSummarizer expenditureSummarizerMock;
+
+    @InjectMocks
+    private MainRunner mainRunner;
+
+    @BeforeEach
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     void run() throws Exception {
@@ -31,6 +47,13 @@ class MainRunnerTest {
         };
 
         MainRunner mainRunner = new MainRunner(expenditureSummarizer);
+        mainRunner.run();
+    }
+
+    @Test
+    void run2() throws Exception {
+        when(expenditureSummarizerMock.getExpenditureSum(Currency.HUF))
+                .thenReturn(150000.0);
         mainRunner.run();
     }
 }
